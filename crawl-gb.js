a81eabd4e2cddd66b435e494aa9995f2a391c2da
bailu7266 <bailu7266@gmail.com>
2018-11-23 00:38:19 +0800

    基本可用

--- crawl-gb.js
+++ crawl-gb.js
@@ -1,8 +1,9 @@
 'use_strict';
 
 const request = require('request-promise');
+const qs = require('qs');
 const cheerio = require('cheerio');
-const URL = require('url-parse');
+// const URL = require('url-parse');
 const fs = require('fs');
 const cookiejar = request.jar();
 
@@ -21,11 +22,12 @@ const rli = readline.createInterface({
 });
 
 // main
-(async() => {
+(async () => {
     let keyWords = process.argv.slice(2);
 
-    while (!keyWords) {
-        keyWords = await inputKeyWords()
+    while (keyWords.length === 0) {
+        let inkey = await inputKeyWords();
+        keyWords = inkey.split(' ');
     }
 
     rli.close();
@@ -33,24 +35,32 @@ const rli = readline.createInterface({
     // let url = new URL(keyWords);
     // let baseUrl = url.protocol + '//' + url.hostname;
 
-    let myRequest = request.defaults({
-        baseUrl: 'https://cn.bing.com',
-        jar: cookiejar, // store cookies
-        resolveWithFullResponse: true,
-        transform: function(body, response) {
-            if (response.statusCode === 200) {
-                console.log(cookiejar.getCookieString(opt.uri));
-                return cheerio.load(body);
-            } else {
-                throw new Error('Transform failed with ret code： ' + response.statusCode);
-            }
-        }
-    });
-
-    let nextUrl = '/search';
+    let nextUrl = 'https://cn.bing.com/search';
 
     try {
-        let $ = await myRequest(nextUrl, { form: { /*form: 'QBLH', qs: 'n', */ q: keyWords.join('+') } })
+        let $ = await request({
+            uri: nextUrl,
+            qs: {
+                q: keyWords.join('+')
+                /*,
+                               qs: 'n',
+                               form: 'QBLH'*/
+            },
+            jar: cookiejar, // store cookies
+            resolveWithFullResponse: true,
+            transform: function (body, response) {
+                if (response.statusCode === 200) {
+                    console.log('Visiting: ' + this.uri + '?' + qs.stringify(this.qs));
+                    // console.log(cookiejar.getCookieString(this.uri));
+                    return cheerio.load(body);
+                } else {
+                    throw new Error(
+                        'Transform failed with ret code： ' + response.statusCode
+                    );
+                }
+            }
+        });
+        // fs.writeFileSync('./tmp/page0.html', $.html());
 
         // collect links from the first SERP
         nextUrl = collectResultLinks($);
@@ -59,30 +69,38 @@ const rli = readline.createInterface({
         process.exit();
     }
 
-    while (nextUrl && (numResultPages < MAX_RESULT_PAGES)) {
+    let myRequest = request.defaults({
+        baseUrl: 'https://cn.bing.com',
+        jar: true, // store cookies
+        transform: (body) => {
+            return cheerio.load(body);
+        }
+    });
+
+    while (nextUrl && numResultPages < MAX_RESULT_PAGES) {
+        numResultPages++;
+
         try {
             let $ = await myRequest(nextUrl);
             // 保持$中的内容，供调试用
-            fs.writeFileSync('1.html', $.html());
+            fs.writeFileSync(`./tmp/${numResultPages}.html`, $.html());
 
             nextUrl = collectResultLinks($);
         } catch (err) {
             console.log(err.message);
             break;
         }
-
-        numResultPages++;
     }
 
     // console log collected links
     resultLinks.forEach((lnk, i) => {
-        console.log('No ' + i + ': ' + lnk);
+        console.log(`No ${i}: ` + lnk);
     });
 })();
 
 function inputKeyWords() {
     return new Promise((resolve, reject) => {
-        rli.question("请输入关键词: ", answer => {
+        rli.question('请输入关键词: ', answer => {
             resolve(answer);
         });
     });
@@ -94,25 +112,21 @@ function collectResultLinks($) {
     // Add the last adv link
     // adLinks.push($('ol#b_results > li.b_ad.b_adBottom > ul > li > div > h2 > a').attr('href'));
 
-    // console.log 一个selector试试看
-    // console.log($.html());
-    let tmp = $("#b_results > li:nth-child(2) > h2 > a");
-    console.log(tmp.html());
-    tmp = $("li.b_algo", "#b_results");
-    tmp = $("a[href^='/']:not(a[href^='//'])", "#b_results").html();
+    var resultItems = $('#b_results > li.b_algo');
+    var pages = $('#b_results > li.b_pag > nav > ul > li');
 
-    let resultItems = $("#b_results > li.b_algo");
-    resultItems.each(($) => {
-        resultLinks.push($('li > h2 > a').attr('href'));
+    resultItems.each((i, item) => {
+        let $ = cheerio.load(item);
+        resultLinks.push($('li h2 > a[h*="ID=SERP"]').attr('href'));
     });
 
-    let pages = $('#b_results > li.b_pag > nav > ul');
-    for (let i = 0; i < pages.length;) {
-        $(pages[i]);
-        i++;
-        if ($('li:has(.sb_pagS.sb_pagS_bg)')) {
-            $ = pages[i];
-            return $('li > a').attr('href');
+    var pageKeys = Object.keys(pages);
+    for (let i = 0; i < pageKeys.length; i++) {
+        let $ = cheerio.load(pages[pageKeys[i]]);
+
+        if ($('a').is('a.sb_pagS.sb_pagS_bp')) {
+            let $ = cheerio.load(pages[pageKeys[i + 1]]);
+            return $('a[h*="ID=SERP"]').attr('href');
         }
     }
 
