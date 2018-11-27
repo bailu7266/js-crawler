5037c20cb835fb61c55af7f8222ebd965e96d6db
bailu7266 <bailu7266@gmail.com>
2018-11-27 16:58:11 +0800

    文件操作改成promise方式

--- crawl-gb.js
+++ crawl-gb.js
@@ -8,6 +8,7 @@ const qs = require('qs');
 const cheerio = require('cheerio');
 // const URL = require('url-parse');
 const fs = require('fs');
+const fsPromises = fs.promises;
 const cookiejar = request.jar();
 const mongoClient = require('mongodb').MongoClient;
 
@@ -26,12 +27,13 @@ var {
 
 var numResultPages = 0;
 var gDb = null;
+var fhJson = null;
 
 // main entry
 (() => {
     // Initialize mongodb database
     const dbUrl = "mongodb://localhost:27017/";
-    mongoClient.connect(dbUrl, (err, db) => {
+    mongoClient.connect(dbUrl, { useNewUrlParser: true }, (err, db) => {
         if (err) {
             console.log(err.message);
         } else {
@@ -44,6 +46,8 @@ var gDb = null;
                     _main(collection)
                         .catch((err) => {
                             console.log(err.message);
+                            fhJson.close();
+                            db.close();
                         })
 
                     // db.close();
@@ -55,7 +59,96 @@ var gDb = null;
 
 async function _main(collection) {
     let keyWords = process.argv.slice(2);
+    if (keyWords.length === 0)
+        keyWords = await inputKeyWords();
 
+    // let url = new URL(keyWords);
+    // let baseUrl = url.protocol + '//' + url.hostname;
+
+    let nextUrl = protocol + '//' + host + searchPath;
+    let qStr = keyWords.join('+');
+
+    let $ = await firstPage(nextUrl, qStr)
+        .catch(err => {
+            console.log(err.message);
+        });
+
+    // collect links from the first SERP
+    nextUrl = crawl($, resultLinks);
+
+    // save links to Json file
+    appendJsonFile(resultLinks);
+
+    // save links to mongodb
+    saveLinks(collection, resultLinks);
+
+    let myRequest = request.defaults({
+        baseUrl: protocol + '//' + host,
+        headers: {
+            'User-Agent': userAgent
+        },
+        proxy: proxy,
+        jar: true, // store cookies
+        transform: body => {
+            return cheerio.load(body);
+        }
+    });
+
+    while (nextUrl && numResultPages < MAX_RESULT_PAGES) {
+        numResultPages++;
+
+        $ = await nextSerp(myRequest, nextUrl)
+            .catch(err => {
+                console.log(err.message);
+            });
+        nextUrl = crawl($, resultLinks);
+
+        appendJsonFile(resultLinks);
+        saveLinks(collection, resultLinks);
+    }
+
+    await report(collection);
+    fhJson.close();
+    gDb.close();
+
+    console.log('Done!!!');
+}
+
+async function appendJsonFile(links) {
+    // 使用异步操作时，会由于优先执行后面的saveLinks，导致resultLinks被清空
+    let strJson = JSON.stringify(links, null, 2);
+    try {
+        if (!fhJson)
+            fhJson = await fsPromises.open('./tmp/links.json', 'w+');
+
+        let offset = 0;
+        let stats = await fhJson.stat();
+        if (stats.size) {
+            // await fh.truncate(stats.size - 1);
+            // let newStats = await fh.stat();
+            // console.log('JSON 文件大小从：' + stats.size + '变成了：' + newStats.size);
+            // Eliminate the last ']'
+            offset = 2;
+        }
+        await fhJson.appendFile(strJson.slice(offset));
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function saveLinks(myCollection, links) {
+    try {
+        let res = await myCollection.insertMany(links);
+        // console.log(`Insert ${res.insertedCount} links to crawl_db.`);
+        links.splice(0, res.insertedCount);
+        // console.log('resultLinks的长度变为：' + links.length);
+    } catch (err) {
+        console.log(err.message);
+        throw err;
+    }
+}
+
+function inputKeyWords() {
     const readline = require('readline');
     if (process.stdin.isTTY) {
         readline.emitKeypressEvents(process.stdin);
@@ -67,27 +160,36 @@ async function _main(collection) {
         output: process.stdout
     });
 
-    while (keyWords.length === 0) {
-        let inkey = await inputKeyWords();
-        keyWords = inkey.split(' ');
-    }
+    return new Promise(async(resolve) => {
+        let line = [];
+        while (line.length === 0) {
+            let inkey = await (() => {
+                return new Promise((res) => {
+                    rli.question('请输入关键词: ', answer => {
+                        res(answer);
+                    });
+                })();
+            });
+            line = inkey.split(' ');
+        }
 
-    rli.close();
+        rli.close();
 
-    // let url = new URL(keyWords);
-    // let baseUrl = url.protocol + '//' + url.hostname;
+        resolve(line);
+    });
+}
 
-    let nextUrl = protocol + '//' + host + searchPath;
+function firstPage(url, qStr) {
+    return new Promise(async(resolve, reject) => {
+        try {
+            if (process.env.SERP_FILE) {
+                resolve(cheerio.load(fs.readFileSync(process.env.SERP_FILE)));
+            }
 
-    try {
-        let $ = null;
-        if (process.env.SERP_FILE) {
-            $ = cheerio.load(fs.readFileSync(process.env.SERP_FILE));
-        } else {
-            $ = await request({
-                uri: nextUrl,
+            let $ = await request({
+                uri: url,
                 qs: {
-                    q: keyWords.join('+')
+                    q: qStr
                 },
                 headers: {
                     'User-Agent': userAgent
@@ -113,82 +215,47 @@ async function _main(collection) {
                         );
                     }
                 }
-            });
-        }
+            })
 
-        // collect links from the first SERP
-        nextUrl = crawl($, resultLinks);
-
-        // save links to mongodb
-        saveLinks(collection, resultLinks);
-    } catch (err) {
-        console.log(err.message);
-        process.exit();
-    }
-
-    let myRequest = request.defaults({
-        baseUrl: protocol + '//' + host,
-        headers: {
-            'User-Agent': userAgent
-        },
-        proxy: proxy,
-        jar: true, // store cookies
-        transform: body => {
-            return cheerio.load(body);
+            resolve($);
+        } catch (err) {
+            console.log(err.message);
+            reject(err);
         }
     });
+}
 
-    while (nextUrl && numResultPages < MAX_RESULT_PAGES) {
-        numResultPages++;
-
+function nextSerp(myRequest, nextUrl) {
+    return new Promise(async(resolve, reject) => {
         try {
             let $ = await myRequest(nextUrl);
             // 保持$中的内容，供调试用
             fs.writeFileSync(`./tmp/${numResultPages}.html`, $.html());
 
-            nextUrl = crawl($, resultLinks);
-
-            saveLinks(collection, resultLinks);
+            resolve($);
         } catch (err) {
             console.log(err.message);
-            break;
+            reject(err);
         }
-    }
-
-    // console log collected links
-    let cursor = collection.find();
-    let bi = 0;
-    do {
-        resultLinks = await cursor.limit(5).toArray();
-        resultLinks.forEach((lnk, i) => {
-            console.log(`No ${i + bi}: ` + lnk.name);
-            console.log(lnk.uri);
-            console.log(lnk.descr);
-            console.log('\n');
-        });
-        bi += 5;
-        cursor = collection.find().skip(bi);
-    } while (resultLinks.length)
-
-    gDb.close();
+    });
 }
 
-async function saveLinks(myCollection, links) {
-    try {
-        let res = await myCollection.insertMany(links);
-        // console.log(`Insert ${res.insertedCount} links to crawl_db.`);
-        links.splice(0, res.insertedCount);
-        // console.log('resultLinks的长度变为：' + links.length);
-    } catch (err) {
-        console.log(err.message);
-        throw err;
-    }
-}
-
-function inputKeyWords() {
-    return new Promise((resolve, reject) => {
-        rli.question('请输入关键词: ', answer => {
-            resolve(answer);
-        });
+function report(collection) {
+    return new Promise(async(resolve) => {
+        // console log collected links
+        let cursor = collection.find();
+        let bi = 0;
+        do {
+            resultLinks = await cursor.limit(5).toArray();
+            resultLinks.forEach((lnk, i) => {
+                console.log(`No ${i + bi}: ` + lnk.name);
+                console.log(lnk.uri);
+                console.log(lnk.descr);
+                console.log('\n');
+            });
+            bi += 5;
+            cursor = collection.find().skip(bi);
+        } while (resultLinks.length)
+        resolve();
     });
 }
