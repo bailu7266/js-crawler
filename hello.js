6b2a11130fdf7493457372b36732d7eff94bd0cb
bailu7266 <bailu7266@gmail.com>
2018-12-01 00:37:58 +0800

    Addon调用JS回调函数

--- hello.js
+++ hello.js
@@ -1,8 +1,18 @@
 const addon = require('./build/Release/n_addon');
 console.log(addon.hello('Tom', 'Jerry', 'McDonald'));
-var obj = { request: "黄河", response: "" };
+var obj = {
+    request: "黄河",
+    response: ""
+};
 console.log('发送前, request = ' + obj.request + '\tresponse = ' + obj.response);
 var ret = addon.testObj(obj);
 console.log('返回码是' + ret);
 if (ret === 0)
     console.log('发送后, request = ' + obj.request + '\tresponse = ' + obj.response);
+
+ret = addon.testCallback((s) => {
+    console.log(s);
+    return 55;
+});
+
+console.log('回调函数返回：' + ret);
