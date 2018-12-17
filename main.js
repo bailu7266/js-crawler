22e8d64e69a840c2406f24559231832bf41cdcfc
bailu7266 <bailu7266@gmail.com>
2018-12-18 00:21:11 +0800

    electron app

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-const { app, BrowserWindow } = require('electron');
+const {
+    app,
+    BrowserWindow
+} = require('electron');
+
+let win = null;
 
 function createWindow() {
-    win = new BrowserWindow({ width: 800, hight: 600 });
+    win = new BrowserWindow({
+        width: 800,
+        hight: 600
+    });
     win.loadFile('welcome.html');
 
     win.webContents.openDevTools();
 
     win.on('closed', () => {
         win = null;
-    })
+    });
 }
 
 app.on('ready', createWindow);
@@ -18,14 +26,14 @@ app.on('window-all-closed', () => {
     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
     // 否则绝大部分应用及其菜单栏会保持激活。
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on('activate', () => {
     // 在macOS上，当单击dock图标并且没有其他窗口打开时，
     // 通常在应用程序中重新创建一个窗口。
     if (win === null) {
-        createWindow()
+        createWindow();
     }
-})
+});
